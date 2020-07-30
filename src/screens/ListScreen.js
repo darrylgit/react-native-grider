1d628cf4cc81ce385bd4343b4a1bc898c49b1704
DanielJWagener <daniel.j.wagener@gmail.com>
2020-07-30 17:42:10 -0400

    Turn list horizontal

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,19 +4,32 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 const ListScreen = () => {
   const friends = [
     { name: 'Friend A' },
-    { name: 'Friend B ' },
-    { name: 'Friend 4' }
+    { name: 'Friend B' },
+    { name: 'Friend 4' },
+    { name: 'Friend 5' },
+    { name: 'Friend 6' },
+    { name: 'Friend 7' },
+    { name: 'Friend 8' },
+    { name: 'Friend 9' },
+    { name: 'Friend 10' }
   ];
   return (
     <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={friend => friend.name}
       data={friends}
       renderItem={({ item }) => {
-        return <Text>{item.name}</Text>;
+        return <Text style={styles.textStyle}>{item.name}</Text>;
       }}
     />
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50
+  }
+});
 
 export default ListScreen;
