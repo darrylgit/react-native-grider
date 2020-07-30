e0770a90d145c36dbf4f35d874b0df800d261584
DanielJWagener <daniel.j.wagener@gmail.com>
2020-07-30 12:36:19 -0400

    Finish JSX exercise

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>This is the component screen</Text>;
+  const name = 'Daniel';
+  return (
+    <View>
+      <Text style={styles.headingStyle}>Getting started with React Native</Text>
+      <Text style={styles.greetingStyle}>My name is {name}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 30
+  headingStyle: {
+    fontSize: 45
+  },
+  greetingStyle: {
+    fontSize: 20
   }
 });
 
