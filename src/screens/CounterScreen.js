ffb5c0bc4521d099fbe0e1dd42b34528fec2e811
DanielJWagener <daniel.j.wagener@gmail.com>
2020-08-01 09:13:29 -0400

    Update counter with hook

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () => {
+  let [counter, updateCounter] = useState(0);
+
   return (
     <View>
-      <Text>Current Count: </Text>
+      <Button
+        title='Increase'
+        onPress={() => {
+          updateCounter(counter + 1);
+        }}
+      />
+      <Button
+        title='Decrease'
+        onPress={() => {
+          updateCounter(counter - 1);
+        }}
+      />
+      <Text>Current Count: {counter}</Text>
     </View>
   );
 };
