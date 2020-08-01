79ca165200f7cd3bd47eba68120da74bacee272c
DanielJWagener <daniel.j.wagener@gmail.com>
2020-08-01 10:01:39 -0400

    Use useState conventions

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -4,20 +4,20 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 import CustomButtom from '../components/CustomButton';
 
 const CounterScreen = () => {
-  let [counter, updateCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
 
   return (
     <View>
       <CustomButtom
         title='Increase'
         callback={() => {
-          updateCounter(counter + 1);
+          setCounter(counter + 1);
         }}
       />
       <CustomButtom
         title='Decrease'
         callback={() => {
-          updateCounter(counter - 1);
+          setCounter(counter - 1);
         }}
       />
       <Text>Current Count: {counter}</Text>
