b0a5c2de0e07af564458ff6ffda7dd944553ae44
DanielJWagener <daniel.j.wagener@gmail.com>
2020-08-01 10:21:36 -0400

    Generate random colors

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import CustomButton from '../components/CustomButton';
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+
   return (
     <View>
-      <Button title='Add a Color' />
+      <CustomButton
+        title='Add a Color'
+        callback={() => {
+          setColors([...colors, randomRgb()]);
+        }}
+      />
+      <View
+        style={{ height: 100, width: 100, backgroundColor: randomRgb() }}
+      ></View>
     </View>
   );
+};
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red}, ${green}, ${blue})`;
 };
 
 const styles = StyleSheet.create({});
