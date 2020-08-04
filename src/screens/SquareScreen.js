37a843229352edb043a52546d8f7d397a542a1b6
DanielJWagener <daniel.j.wagener@gmail.com>
2020-08-04 09:24:40 -0400

    Add actual color square

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -3,6 +3,8 @@ import { View, Text } from "react-native";
 
 import ColorCounter from "../components/ColorCounter";
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
@@ -13,20 +15,27 @@ const SquareScreen = () => {
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setRed(red + COLOR_INCREMENT)}
+        onDecrease={() => setRed(red - COLOR_INCREMENT)}
         color='Red'
       />
       <ColorCounter
-        onIncrease={() => setGreen(green + 1)}
-        onDecrease={() => setGreen(green - 1)}
+        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
         color='Green'
       />
       <ColorCounter
-        onIncrease={() => setBlue(blue + 1)}
-        onDecrease={() => setBlue(blue - 1)}
+        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
         color='Blue'
       />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red}, ${green}, ${blue})`
+        }}
+      ></View>
     </View>
   );
 };
