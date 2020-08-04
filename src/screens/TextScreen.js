fa7193f92dc77750f5a23d0ac5493e1a29aeee2e
DanielJWagener <daniel.j.wagener@gmail.com>
2020-08-04 14:18:57 -0400

    Add TextInput

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
   return (
     <View>
-      <Text>Text Screen</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    padding: 10,
+    borderColor: "black",
+    borderWidth: 1
+  }
+});
 
 export default TextScreen;
