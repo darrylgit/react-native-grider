5d04ce33ce454f0842a8812ec680bab45c6932a0
DanielJWagener <daniel.j.wagener@gmail.com>
2020-08-08 15:55:40 -0400

    Add flexbox styles

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,20 +4,35 @@ import { Text, View, StyleSheet } from "react-native";
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   viewStyle: {
-    borderWidth: 1,
-    borderColor: "black"
+    borderWidth: 3,
+    borderColor: "black",
+    height: 200,
+    alignItems: "center"
   },
-  textStyle: {
-    borderWidth: 10,
+  textOneStyle: {
+    borderWidth: 3,
     borderColor: "red",
-    margin: 20
+    flex: 4
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    flex: 4,
+    borderColor: "red",
+    alignSelf: "flex-start"
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+    flex: 2
   }
 });
 
